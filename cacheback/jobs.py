6fada5269b7c660be1b8f679ca583480a8b9da70
Ujjwal-Squadstack <ujjwal.gupta@squadstack.com>
2024-03-19 00:13:10 +0530

    fix list issue

--- cacheback/jobs.py
+++ cacheback/jobs.py
@@ -75,7 +75,7 @@ class QuerySetJob(Job):
     def get_init_kwargs(self):
         app_label, model_name = self.model._meta.app_label, self.model._meta.model_name
 
-        return {'model': [app_label, model_name],
+        return {'model': self.model,
                 'lifetime': self.lifetime,
                 'cache_alias': self.cache_alias}
 
